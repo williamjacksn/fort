0d753826f919af0301f563434e29fddf02da4054
William Jackson <william@subtlecoolness.com>
2020-08-07 13:59:25 -0500

    Add a connection pool to PostgresDatabase

--- fort/postgres.py
+++ fort/postgres.py
@@ -1,25 +1,28 @@
 import logging
 import psycopg2
 import psycopg2.extras
+import psycopg2.pool
 
 from typing import Any, Dict, List, Optional
 
 
 class PostgresDatabase:
-    def __init__(self, dsn):
+    def __init__(self, dsn, minconn=1, maxconn=1):
         self.log = logging.getLogger(__name__)
-        self.cnx = psycopg2.connect(dsn, cursor_factory=psycopg2.extras.DictCursor)
-        self.cnx.autocommit = True
+        self.p = psycopg2.pool.ThreadedConnectionPool(minconn, maxconn, dsn, cursor_factory=psycopg2.extras.DictCursor)
         psycopg2.extras.register_uuid()
 
     def q(self, sql: str, params: Dict = None) -> List[Dict]:
         """Execute a query and return all rows"""
         if params is None:
             params = {}
-        with self.cnx.cursor() as c:
+        cnx = self.p.getconn()
+        with cnx.cursor() as c:
             self.log.debug(c.mogrify(sql, params).decode())
             c.execute(sql, params)
-            return c.fetchall()
+            result = c.fetchall()
+        self.p.putconn(cnx)
+        return result
 
     def q_one(self, sql: str, params: Dict = None) -> Optional[Dict]:
         """Execute a query and return the first row, or None if there are no rows"""
@@ -37,7 +40,10 @@ class PostgresDatabase:
         """Execute a statement and return the number of rows affected"""
         if params is None:
             params = {}
-        with self.cnx.cursor() as c:
+        cnx = self.p.getconn()
+        with cnx.cursor() as c:
             self.log.debug(c.mogrify(sql, params).decode())
             c.execute(sql, params)
-            return c.rowcount
+            result = c.rowcount
+        self.p.putconn(cnx)
+        return result
