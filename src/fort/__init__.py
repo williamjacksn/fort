bc61298fc403667885380e3f92cf79a3120a26c6
William Jackson <565174+williamjacksn@users.noreply.github.com>
2025-08-13 13:20:31 -0500

    Fix F401

--- src/fort/__init__.py
+++ src/fort/__init__.py
@@ -1,8 +1,8 @@
 import warnings
 
 try:
-    from .postgres import PostgresDatabase
+    from .postgres import PostgresDatabase as PostgresDatabase
 except ModuleNotFoundError:
     warnings.warn("PostgresDatabase will not be available", ImportWarning)
 
-from .sqlite import SQLiteDatabase
+from .sqlite import SQLiteDatabase as SQLiteDatabase
